d9e0095e87bac92d189d02904395b60ba1d36213
Hafidzrdwn <hafidz10032005@gmail.com>
2022-07-30 22:31:39 +0700

    add input count

--- get-quote.py
+++ get-quote.py
@@ -1,16 +1,24 @@
 import random
 
 
+def remove_newlines(fname):
+    flist = open(fname).readlines()
+    return [s.rstrip("\n") for s in flist]
+
+
 def main():
-    # print("Keep it logically awesome.")
+    quotes = remove_newlines("quotes.txt")
+    print("Welcome to the Random Quote Generator!")
+    count = input("How many quotes would you like to generate? ")
 
-    f = open("quotes.txt")
-    quotes = f.readlines()
-    f.close()
+    for i in range(0, int(count)):
+        print(f"{i + 1}. {random.choice(quotes)}")
 
-    last = 13
-    rnd = random.randint(0, last)
-    print(quotes[rnd])
+    print("-" * 50)
+    print("Thanks for using the Random Quote Generator!")
+    again = input("Would you like to generate another quote? (y/n) ")
+    if again == "y":
+        main()
 
 
 if __name__ == "__main__":
